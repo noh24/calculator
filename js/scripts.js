b330f946a5cf987a047955cf9e9e56d90fb5627a
Brian Noh <noh24@ymail.com>
2022-10-20 13:48:13 -0400

    Add one of organizing js code

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,56 @@ function divide(num1, num2) {
 
 
 //UI (User Interface)
-const num1 = parseFloat(prompt("Enter a number:"));
-const num2 = parseFloat(prompt("Enter another number:"));
+function handleCalculation(e) {
+  e.preventDefault();
+  //code to get user input value and process form values will go here!
+  const num1 = parseFloat(document.getElementById("input1").value);
+  console.log("num1:", num1);
+  const num2 = parseFloat(document.getElementById("input2").value);
+  console.log("num2:", num2);
+  const operator = document.querySelector("input[name='operator']:checked").value;
+  console.log("checked value:", operator)
 
-const sum = ("The result of " + num1 + " + " + num2 + " = " + add(num1, num2) + ". ");
-const sub = ("The result of " + num1 + " - " + num2 + " = " + subtract(num1, num2) + ". ");
-const times = ("The result of " + num1 + " * " + num2 + " = " + multiply(num1, num2) + ". ");
-const division = ("The result of " + num1 + " / " + num2 + " = " + divide(num1, num2) + ". ");
+  //if statement to start calculating
+  let result;
+  if (operator === "add") {
+    result = add(num1, num2);
+  } else if (operator === "subtract") {
+    result = subtract(num1, num2);
+  } else if (operator === "multiply") {
+    result = multiply(num1, num2);
+  } else if (operator === "divide") {
+    result = divide(num1, num2);
+  }
+  document.getElementById("output").innerText = result;
+}
 
-window.alert(sum + sub + times + division);
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const num1 = parseFloat(prompt("Enter a number:"));
+// const num2 = parseFloat(prompt("Enter another number:"));
+
+// const sum = ("The result of " + num1 + " + " + num2 + " = " + add(num1, num2) + ". ");
+// const sub = ("The result of " + num1 + " - " + num2 + " = " + subtract(num1, num2) + ". ");
+// const times = ("The result of " + num1 + " * " + num2 + " = " + multiply(num1, num2) + ". ");
+// const division = ("The result of " + num1 + " / " + num2 + " = " + divide(num1, num2) + ". ");
+
+// window.alert(sum + sub + times + division);
